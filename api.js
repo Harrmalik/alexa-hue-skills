2711eaa7a5ffd5de2bd508217d1f5cebc7f75748
Malik Harrison <harrmalik@gmail.com>
2019-05-13 15:48:21 -0400

    working app

--- api.js
+++ api.js
@@ -1,19 +1,26 @@
-var express = require('express');
-var app = express();
-var path = require('path');
-var http = require('http').Server(app);
-var fs = require('fs');
-var fetch = require('node-fetch');
+const express = require('express');
+const app = express();
+const path = require('path');
+const http = require('http').Server(app);
+const fs = require('fs');
+const fetch = require('node-fetch');
+const cors = require('cors');
+const map = require('lodash/map');
+const https = require("https");
+const moment = require("moment");
+// Variables
+const hueIP = 'http://192.168.0.18';
+const hueUser = 'gDiIztNg3YZOQF3ASNLHlrDj7SppTwLT-12-C-cs';
+const apiUrl = `${hueIP}/api/${hueUser}`
+let activatedLights = [];
+let sensedPresence = false;
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json())
-var cors = require('cors');
-
 app.use(cors())
 
-var hueIP = 'http://192.168.0.18';
-var hueUser = 'gDiIztNg3YZOQF3ASNLHlrDj7SppTwLT-12-C-cs';
-var apiUrl = `${hueIP}/api/${hueUser}`
 
+// Routes
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + 'public/index.html'));
 });
@@ -72,6 +79,63 @@ app.get('/getSensors', (req, res) => {
     getData(`${apiUrl}/sensors/${sensor}`);
 })
 
+app.get('/updateLight/:light', (req, res) => {
+    let light = req.params.light ? req.params.light : '',
+        paramKeys = Object.keys(req.query),
+        lightbulb = activatedLights.find(l => l.name.toLowerCase() == light)
+
+    // Cleans data for hue api
+    for (var i = 0; i < paramKeys.length; i++) {
+        let param = req.query[paramKeys[i]]
+        if (paramKeys[i].match(/hue|sat|bri|^ct$/))
+            req.query[paramKeys[i]] = parseInt(req.query[paramKeys[i]])
+
+        if (param === 'true')
+            req.query[paramKeys[i]] = true
+
+        if (param === 'false')
+            req.query[paramKeys[i]] = false
+    }
+
+    if (lightbulb) {
+        light = lightbulb.id
+    }
+
+    const getData = async url => {
+      try {
+          console.log('start');
+        await timeout(alexaTimeConversion(req.query.time));
+        console.log('timeout');
+        const response = await fetch(url, {
+            method: 'put',
+            body: JSON.stringify(req.query)
+        });
+        const json = await response.json();
+        console.log(json);
+        return res.json(json)
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getData(`${apiUrl}/lights/${light}/state`);
+})
+
+app.get('/checkWhosHome', (req, res) => {
+    const getData = async url => {
+      try {
+        const response = await fetch(url);
+        const json = await response.json();
+        console.log(json);
+        return res.json({
+            malik: json.state.presence
+        })
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getData(`${apiUrl}/sensors/10`);
+})
+
 app.put('/updateLight/:light', (req, res) => {
     let light = req.params.light ? req.params.light : ''
 
@@ -110,7 +174,42 @@ app.put('/updateGroup/:group', (req, res) => {
     getData(`${apiUrl}/groups/${group}/action`);
 })
 
+setInterval(() => {
+    const getData = async url => {
+      try {
+        const response = await fetch(url);
+        const json = await response.json();
+        sensedPresence = json.state.presence
+        //TODO: Do something when motion sensor is activated
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getData(`${apiUrl}/sensors/3/`);
+},10000)
+
+
+function timeout(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+let alexaTimeConversion = time => {
+    let delay = time.split('PT')[1]
+    if (!delay) {
+        return 0
+    } else {
+        if (delay.match(/H/))
+            return delay.split('H')[0] * 3600000;
+
+        if (delay.match(/M/))
+            return delay.split('M')[0] * 60000;
+
+        if (delay.match(/S/))
+            return delay.split('S')[0] * 1000;
+    }
+}
 
 http.listen(3001, function(){
+
   console.log('listening on *:3001');
 });
---
+++ index.js
@@ -0,0 +1,199 @@
+/* eslint-disable  func-names */
+/* eslint-disable  no-console */
+
+const Alexa = require('ask-sdk');
+const https = require("https");
+const url = 'https://d2dc9193.ngrok.io';
+
+const WelcomeHandler = {
+  canHandle(handlerInput) {
+    const request = handlerInput.requestEnvelope.request;
+    return request.type === 'LaunchRequest';
+  },
+  handle(handlerInput) {
+    // DO STUFF HERE
+
+    return handlerInput.responseBuilder
+      .speak('welcome dawg')
+      .withSimpleCard(SKILL_NAME, 'Playing with lights')
+      .getResponse();
+  },
+};
+
+const OnHandler = {
+  canHandle(handlerInput) {
+    const request = handlerInput.requestEnvelope.request;
+    return (request.type === 'IntentRequest'
+        && request.intent.name === 'OnIntent');
+  },
+  handle(handlerInput) {
+    console.log(handlerInput.requestEnvelope.request.intent);
+    const lightSlot = handlerInput.requestEnvelope.request.intent.slots.light;
+    const colorSlot = handlerInput.requestEnvelope.request.intent.slots.color;
+    const timeSlot = handlerInput.requestEnvelope.request.intent.slots.time;
+    let light,color,colorParam = '',timeParam = '';
+
+    if (lightSlot) {
+      light = lightSlot.value;
+    }
+
+    if (colorSlot) {
+      let colorInfo = colors.find(c => c.color == colorSlot.value);
+      if (colorInfo) {
+        color = colorInfo.hue;
+        colorParam = `&hue=${color}`;
+      }
+    }
+
+    if (timeSlot) {
+      timeParam = `&time=${timeSlot.value}`;
+    }
+
+    console.log(colorParam);
+    console.log(timeParam);
+
+    https.get(`${url}/updateLight/${light}?on=true${colorParam}${timeParam}`, res => {
+      res.setEncoding("utf8");
+      let body = "";
+      res.on("data", data => {
+        body += data;
+      });
+      res.on("end", () => {
+        body = JSON.parse(body);
+        console.log(body);
+      });
+    });
+
+    return handlerInput.responseBuilder
+      .speak(`
+<speak>
+    Welcome to Car-Fu.
+    <audio src="soundbank://soundlibrary/transportation/amzn_sfx_car_accelerate_01" />
+    You can order a ride, or request a fare estimate.
+    Which will it be?
+</speak>
+      `)
+      .withSimpleCard(light, `Turned on ${light}`)
+      .getResponse();
+  },
+};
+
+const OffHandler = {
+  canHandle(handlerInput) {
+    const request = handlerInput.requestEnvelope.request;
+    return (request.type === 'IntentRequest'
+        && request.intent.name === 'OffIntent');
+  },
+  handle(handlerInput) {
+    console.log(handlerInput.requestEnvelope.request.intent);
+    const light = handlerInput.requestEnvelope.request.intent.slots.light.value;
+    const timeSlot = handlerInput.requestEnvelope.request.intent.slots.time;
+    let timeParam = '';
+
+    if (timeSlot) {
+      timeParam = `&time=${timeSlot.value}`;
+    }
+
+    https.get(`${url}/updateLight/${light}?on=false${timeParam}`, res => {
+      res.setEncoding("utf8");
+      let body = "";
+      res.on("data", data => {
+        body += data;
+      });
+      res.on("end", () => {
+        body = JSON.parse(body);
+        console.log(body);
+      });
+    });
+
+    return handlerInput.responseBuilder
+      .speak(`Turned off ${light}`)
+      .reprompt('Will that be all for today sir.')
+      .withSimpleCard(light, `Turned off ${light}`)
+      .getResponse();
+  },
+};
+
+const HelpHandler = {
+  canHandle(handlerInput) {
+    const request = handlerInput.requestEnvelope.request;
+    return request.type === 'IntentRequest'
+      && request.intent.name === 'AMAZON.HelpIntent';
+  },
+  handle(handlerInput) {
+    return handlerInput.responseBuilder
+      .speak(HELP_MESSAGE)
+      .reprompt(HELP_REPROMPT)
+      .getResponse();
+  },
+};
+
+const ExitHandler = {
+  canHandle(handlerInput) {
+    const request = handlerInput.requestEnvelope.request;
+    return request.type === 'IntentRequest'
+      && (request.intent.name === 'AMAZON.CancelIntent'
+        || request.intent.name === 'AMAZON.StopIntent');
+  },
+  handle(handlerInput) {
+    return handlerInput.responseBuilder
+      .speak(STOP_MESSAGE)
+      .getResponse();
+  },
+};
+
+const SessionEndedRequestHandler = {
+  canHandle(handlerInput) {
+    const request = handlerInput.requestEnvelope.request;
+    return request.type === 'SessionEndedRequest';
+  },
+  handle(handlerInput) {
+    console.log(`Session ended with reason: ${handlerInput.requestEnvelope.request.reason}`);
+
+    return handlerInput.responseBuilder.getResponse();
+  },
+};
+
+const ErrorHandler = {
+  canHandle() {
+    return true;
+  },
+  handle(handlerInput, error) {
+    console.log(`Error handled: ${error.message}`);
+
+    return handlerInput.responseBuilder
+      .speak('Sorry, an error occurred.')
+      .reprompt('Sorry, an error occurred.')
+      .getResponse();
+  },
+};
+
+const SKILL_NAME = 'Hue App Demo';
+const HELP_MESSAGE = 'You can turn on or off lights by saying turn on table';
+const HELP_REPROMPT = 'What can I help you with?';
+const STOP_MESSAGE = 'Goodbye!';
+
+const colors = [
+  { color: 'blue', hue: 43237},
+  { color: 'green', hue: 25000},
+  { color: 'yellow', hue: 10911},
+  { color: 'purple', hue: 50000},
+  { color: 'orange', hue: 7266},
+  { color: 'pink', hue: 60000},
+  { color: 'red', hue: 70000},
+  { color: 'white', hue: 41283}
+];
+
+const skillBuilder = Alexa.SkillBuilders.standard();
+
+exports.handler = skillBuilder
+  .addRequestHandlers(
+    WelcomeHandler,
+    OnHandler,
+    OffHandler,
+    HelpHandler,
+    ExitHandler,
+    SessionEndedRequestHandler
+  )
+  .addErrorHandlers(ErrorHandler)
+  .lambda();
